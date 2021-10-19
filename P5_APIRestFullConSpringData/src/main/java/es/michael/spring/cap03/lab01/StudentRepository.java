a896a1e6977da7a67ecbd7ae9c9e358b04261457
MichaelSteria <michael-laudrup.luis@soprasteria.com>
2021-10-19 09:29:59 +0200

    ds

--- P5_APIRestFullConSpringData/src/main/java/es/michael/spring/cap03/lab01/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package es.michael.spring.cap03.lab01;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface StudentRepository extends JpaRepository<StudentJPA,Integer> {
-	
-}
